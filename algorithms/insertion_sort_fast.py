ee71c7d799cc59ead6915038ecc3225d712beca8
Parisa Mousavi <43146130+parisam83@users.noreply.github.com>
2019-05-20 12:40:24 +0430

    Create insertion_sort_fast.py

---
+++ algorithms/insertion_sort_fast.py
@@ -0,0 +1,17 @@
+'''This insertion sort algorithm 
+   which is wrote by me(Parisa Mousavi)
+   is faster and much more easier to 
+   understand for beginners...'''
+   
+   
+def insertion_sort_fast(arr):
+  for i in range(1, len(arr)):
+      j = i - 1
+      for k in range(i):
+          if arr[i] < arr[j]:
+              arr[i], arr[j] = arr[j], arr[i]
+              i = i -1
+              j = j - 1
+
+#insertion_sort_fast(arr)
+#print(arr)
